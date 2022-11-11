00cfa44a76abec5a18ca1d6065f9114c4462f294
Thomas Hervieu <tom.hervieu@gmail.com>
2022-11-11 11:32:19 -0500

    Implement getOutcome() in a simplistic way
    If statements can be reduced by using modulus, but they will be kept
    for the sake of the project instructions.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -3,7 +3,44 @@ options = ['rock', 'paper', 'scissors'];
 
 function getComputerChoice(){
     let selection = Math.floor(Math.random()*3);
-    console.log(selection);
+    return options[selection];
 }
 
-getComputerChoice();
+function getOutcome(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+
+    // console.log(`playerselection: ${playerSelection}\ncomputerselection: ${computerSelection}`);
+
+    //this is intentionally implemented in a manual way.
+    //Otherwise, would think about this problem in cycles using modulus operator.
+    if (playerSelection == 'rock') {
+        if (computerSelection == 'paper'){
+            return(`You Lose! Paper beats rock :(`);
+        } else if (computerSelection == 'scissors') {
+            return(`You Win! Rock beats scissors`);
+        } else {
+            return(`It's a tie!`);
+        }
+    }
+    if (playerSelection == 'paper') {
+        if (computerSelection == 'scissors'){
+            return(`You Lose! Scissors beats paper :(`);
+        } else if (computerSelection == 'rock') {
+            return(`You Win! Paper beats rock`);
+        } else {
+            return(`It's a tie!`);
+        }
+    }
+    if (playerSelection == 'scissors') {
+        if (computerSelection == 'rock'){
+            return(`You Lose! Rock beats scissors :(`);
+        } else if (computerSelection == 'paper') {
+            return(`You Win! Scissors beats paper`);
+        } else {
+            return(`It's a tie!`);
+        }
+    }
+
+}
+
+// console.log(getOutcome('scissors',getComputerChoice()));
